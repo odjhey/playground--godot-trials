ceaecbd90f503a50c7b4264ff4a2fca74276a2c6
odjhey <odjhey@gmail.com>
2023-02-09 05:34:22 +0800

    feat: add player health

--- turnbased/src/Battle.gd
+++ turnbased/src/Battle.gd
@@ -4,12 +4,18 @@ extends Control
 signal textbox_closed
 
 func _ready():
+	set_health($PlayerPanel/PlayerData/ProgressBar, State.current_health, State.max_health)
 	$TextBox.hide()
 	$ActionsPanel.hide()
 
 	display_text("A wild enemy appears!")
 	yield(self, "textbox_closed")
 	$ActionsPanel.show()
+
+func set_health(progress_bar, health, max_health):
+	progress_bar.value = health
+	progress_bar.max_value = max_health
+	progress_bar.get_node("Label").text = "HP: %d/%d" % [health, max_health]
 
 func _input(_event):
 	if (Input.is_action_just_pressed("ui_accept") or Input.is_mouse_button_pressed(BUTTON_LEFT)) and $TextBox.visible:
@@ -23,5 +29,5 @@ func display_text(text):
 func _on_Run_pressed():
 	display_text("Got away safely!")
 	yield(self, "textbox_closed")
-	yield(get_tree().create_timer(0.5), 'timeout')
+	yield(get_tree().create_timer(0.25), 'timeout')
 	get_tree().quit()
